26319e42ac5cf2ba033184e8e28d62d5b61c5a90
Salamata Bah <sb4255@drexel.edu>
2022-07-06 11:18:58 -0700

    project structure and homepage components

---
+++ capstone-ui/src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../../src/components/Navbar/Navbar";
+import "./App.css";
+
+const App = () => {
+  console.log("test");
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+};
+export default App;
---
+++ capstone-ui/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Navbar.css";
+import Logo from "../Logo/Logo";
+
+const Navbar = () => {
+  console.log("nav");
+  return (
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="navbar-logo">
+          <Logo />
+        </div>
+        <div className="navbar-container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#about">About</a>
+          </p>
+          <p>
+            <a href="#faqs">FAQs</a>
+          </p>
+          <p>
+            <a href="#successStories">Success Stories</a>
+          </p>
+          <p>
+            <a href="#contact">Contact</a>
+          </p>
+        </div>
+        <div className="navbar-login">
+          <button type="button"> Sign up </button>
+          <button type="button"> Log in </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
