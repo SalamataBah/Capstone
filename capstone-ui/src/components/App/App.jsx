f2c3112cde90a2b597440e1f0c4f491691b4b285
Salamata Bah <sb4255@drexel.edu>
2022-07-08 10:47:26 -0700

    rm node modules

--- capstone-ui/src/components/App/App.jsx
+++ capstone-ui/src/components/App/App.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
-  console.log("test");
   return (
     <div className="App">
       <BrowserRouter>
