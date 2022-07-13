9c69f91049886ea12aed13fdcc79cf6c95324e3c
Salamata Bah <sb4255@drexel.edu>
2022-07-13 09:19:19 -0700

    backend with Parse instead of backend for Linkedin API

--- capstone-backend/app.js
+++ capstone-backend/app.js
@@ -1,42 +1,39 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var callback = require('./routes/callback');
-var app = express();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Parse = require('parse/node')
+const {PARSE_APP_ID, PARSE_JAVASCRIPT_KEY} = require('./config')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const app = express()
+const port = process.env.PORT || 3001
 
+app.use(express.json())
+app.use(morgan("tiny"))
+app.use(cors())
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'capstone-ui/public')));
+Parse.initialize(PARSE_APP_ID, PARSE_JAVASCRIPT_KEY)
+Parse.serverURL = "https://parseapi.back4app.com"
 
-app.use(express.static(path.join(__dirname, 'capstone-ui/src')));
-app.use('/callback', callback);
+app.post('/register', async (req, res) => {
+  let user = new Parse.User(req.body)
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'capstone-ui/public', 'index.html'))
+  try {
+      await user.signUp()
+      res.status(201)    
+      res.send({"user" : user})
+  } catch (error) {
+      res.status(400)
+      res.send({"error" : "Failed to create user: " + error })
+  }
 })
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.post('/login', async (req, res) => {
+  try {
+    const user = await Parse.User.logIn(req.body.username, req.body.password)
+    res.send({"user" : user})
+  } catch (error) {
+    res.status(400)
+    res.send({"error" : "Login failed: " + error })
+  }
+})
 module.exports = app;
