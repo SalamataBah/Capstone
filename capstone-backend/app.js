ea7174eaf30f3020d6db9b6928fb561604f3c8d4
Salamata Bah <sb4255@drexel.edu>
2022-07-18 14:58:06 -0700

    debug LinkedinLogin

--- capstone-backend/app.js
+++ capstone-backend/app.js
@@ -16,6 +16,8 @@ Parse.serverURL = "https://parseapi.back4app.com"
 
 app.post('/register', async (req, res) => {
   let user = new Parse.User(req.body)
+  console.log('user: ', user);
+  console.log('req.body: ', req.body);
 
   try {
       await user.signUp()
@@ -36,4 +38,37 @@ app.post('/login', async (req, res) => {
     res.send({"error" : "Login failed: " + error })
   }
 })
+// Set up Linkedin OAuth here 
+var router = express.Router();
+const request = require('superagent');
+
+router.get('/profile', function(req, res, next){
+  requestAccessToken(req.body.code, req.query.state)
+  .then(response => {
+    requestProfile(response.body.access_token)
+    .then(response =>{
+      console.log('response: ', response.body);
+
+      res.render('profile', {profile: response.body})
+    } );
+  })
+  .catch((error) => {
+    res.status(500).send(`${error}`)
+  })
+})
+
+function requestAccessToken(code, state){
+  return request.post('https://www.linkedin.com/oauth/v2/accessToken')
+    .send('grant_type=authorization_code')
+    .send('redirect_uri= http://localhost:3000/profile')
+    .send('client_id=86bj0k3tnixpok')
+    .send('client_secret=9XSEs7KoTrAeu78b')
+    .send(`code=${code}`)
+    .send(`state=${state}`)
+}
+
+function requestProfile(token) {
+  return request.get('https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName,profilePicture(displayImage~digitalmediaAsset:playableStreams))')
+  .set('Authorization', `Bearer ${token}`)
+}
 module.exports = app;
